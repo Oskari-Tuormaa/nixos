75122bc7b4580fb77d302c0a067d2dfb09826e3e
Oskari Kristian Tuormaa <oskaritu@gmail.com>
2026-03-15 00:57:11 +0100

    feat: Add hopper configuration

--- hosts/hopper/default.nix
+++ hosts/hopper/default.nix
@@ -5,7 +5,7 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/common
-    ../../modules/features/nvidia.nix
+    # ../../modules/features/nvidia.nix # TODO: Fix nvidia
     ../../modules/features/desktop.nix
     ../../modules/features/desktop-i3.nix
   ];
@@ -16,4 +16,10 @@
   nixpkgs.config.allowUnfree = true;
 
   # Laptop-specific optimizations can go here
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.xserver.dpi = 200;
+
+  # TODO: Fix DPI scaling in kitty
 }
