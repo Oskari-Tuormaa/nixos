f63521fd258323f033e302a439a4c3cafdf120b7
Oskari Kristian Tuormaa <oskaritu@gmail.com>
2026-03-14 23:03:34 +0100

    feat: Add launch VM hotkey to .nvim-workspace

--- .nvim-workspace/workspace.lua
+++ .nvim-workspace/workspace.lua
@@ -1,3 +1,10 @@
 local nmap = require("config.utils").nmap
 
+local hostname = vim.fn.hostname()
+
+local clean_vm = "rm "..hostname..".qcow2"
+local build_vm = "nixos-rebuild build-vm --flake .\\#"..hostname
+local run_vm = "./result/bin/run-"..hostname.."-vm"
+
 nmap("<leader>e", "<cmd>sp term://nix flake check<cr>")
+nmap("<leader>vm", "<cmd>sp term://"..clean_vm.."; "..build_vm.." && "..run_vm.."<cr>")
