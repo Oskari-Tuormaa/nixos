ff81cbf69f78a224cdda07988fbf5b0173c8f081
Oskari Kristian Tuormaa <oskaritu@gmail.com>
2026-03-15 02:25:38 +0100

    feat: Add hopper public key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -10,13 +10,16 @@ let
   # Run on each host: cat /etc/ssh/ssh_host_ed25519_key.pub
   # TODO: Add hosts
   lovelace = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwcUYz2Y/F9yH2M8E5bUAqqyBA5aYIB8iOgqCpAWF4U";
+  hopper = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXND570ya9YurMACE2hHbAhU+SSR28MGJjfWBtK9vBw";
 
   # Groups of keys for easy management
   allHosts = [
     lovelace
+    hopper
   ];
   desktopHosts = [
     lovelace
+    hopper
   ];
   # serverHosts = [ perlman ];
 
