ad875daabefedf6a75dc6ecd44d6c1636b6edfc7
Oskari Kristian Tuormaa <oskaritu@gmail.com>
2026-09-08 00:02:40 +0200

    fix: Configure correct danish locale

--- modules/common/settings.nix
+++ modules/common/settings.nix
@@ -8,6 +8,17 @@
   # Locale and timezone - customize as needed
   time.timeZone = "Europe/Copenhagen";
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "da_DK.UTF-8";
+    LC_IDENTIFICATION = "da_DK.UTF-8";
+    LC_MEASUREMENT = "da_DK.UTF-8";
+    LC_MONETARY = "da_DK.UTF-8";
+    LC_NAME = "da_DK.UTF-8";
+    LC_NUMERIC = "da_DK.UTF-8";
+    LC_PAPER = "da_DK.UTF-8";
+    LC_TELEPHONE = "da_DK.UTF-8";
+    LC_TIME = "da_DK.UTF-8";
+  };
 
   # Enable networking by default
   networking.useDHCP = lib.mkDefault true;
